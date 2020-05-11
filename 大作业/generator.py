2050204c732d4a320a9b0c0780fb8da444e158fe
Guo Zhuoning <gzn00417@foxmail.com>
2020-05-11 19:51:33 +0800

    Create generator.py

---
+++ 大作业/generator.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+import random
+
+N = 1000
+M = 100
+filename = ".\data2.txt"
+with open(filename, "w", encoding="utf-8") as file:
+    for i in range(N):
+        file.write(str(i + 1) + " " + str(round((random.random() - 0.5) * M, 3)) + "\n")
