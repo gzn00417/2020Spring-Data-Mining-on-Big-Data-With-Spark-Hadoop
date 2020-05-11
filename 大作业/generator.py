592c98ee5e3b88f7027bf258455a42ddc4e1fa5f
Guo Zhuoning <gzn00417@foxmail.com>
2020-05-11 19:52:58 +0800

    Update generator.py

--- 大作业/generator.py
+++ 大作业/generator.py
@@ -6,4 +6,6 @@ M = 100
 filename = ".\data2.txt"
 with open(filename, "w", encoding="utf-8") as file:
     for i in range(N):
-        file.write(str(i + 1) + " " + str(round((random.random() - 0.5) * M, 3)) + "\n")
+        file.write(
+            "(" + str(i + 1) + ", " + str(round((random.random() - 0.5) * M, 3)) + ")\n"
+        )
